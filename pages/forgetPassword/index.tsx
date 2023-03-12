e06d7e36204865cb5a576d6097e00a69a2ff3966
sefakrb <sefa799@gmail.com>
2023-03-12 15:43:35 +0300

    initialize forgetPassword

---
+++ pages/forgetPassword/index.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+
+export default function ForgetPassword() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Grid columns={12} style={{ display: "flex", justifyContent: "center" }}>
+        FORGET PASSWORD
+      </Grid>
+    </div>
+  );
+}
