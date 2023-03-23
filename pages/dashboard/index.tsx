325e1db475556dbd85ed74fa04ac02d2a58e77b3
sefakrb <sefa799@gmail.com>
2023-03-23 22:16:28 +0300

    add auth control

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,15 @@
 import { Grid } from "@mui/material";
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Dashboard() {
+  const { status, data } = useSession();
+  const router = useRouter();
+
+  if (status === "unauthenticated") {
+    router.push("/auth");
+  }
+
   return (
     <div style={{ height: "100vh" }}>
       <Grid columns={12} style={{ display: "flex", justifyContent: "center" }}>
