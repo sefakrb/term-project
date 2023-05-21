022bbcbe87114bf8e345cf58e61cea590968fac1
sefakrb <sefa799@gmail.com>
2023-05-22 02:25:51 +0300

    add .env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,18 @@ import 'sweetalert2/dist/sweetalert2.min.css'
 import { Open_Sans } from '@next/font/google'
 
 import type { AppProps } from 'next/app'
+import { StyledEngineProvider } from '@mui/material'
 
 const inter = Open_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
       <SessionProvider session={pageProps.session}>
-         <div className={inter.className}>
-            <Component {...pageProps} />
-         </div>
+         <StyledEngineProvider injectFirst>
+            <div className={inter.className}>
+               <Component {...pageProps} />
+            </div>
+         </StyledEngineProvider>
       </SessionProvider>
    )
 }
--- pages/api/api.ts
+++ pages/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080/'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const axiosInstance = axios.create({
    baseURL: BASE_URL,
